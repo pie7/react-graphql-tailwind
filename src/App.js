c6d6da2dcbb389f42e8f765bbb97770168e7aa1d
pie7 <j3pxy6@gmail.com>
2022-06-17 16:37:55 +0800

    feat: add Upcoming page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import CrewTable from "./components/CrewTable";
 import Crews from "./components/Crews";
 import Launches from "./components/Launches";
+import { useEffect, useState } from "react";
+import WebVitasBox from "./components/WebVitasBox";
+import Upcoming from "./components/Upcoming";
 
 const CrewPage = () => {
   return (
-    <div className="max-w-screen-sm mx-auto">
+    <div className="max-w-fit mx-auto">
       <div className="mt-10">
         <Crews />
       </div>
@@ -17,13 +20,40 @@ const CrewPage = () => {
   );
 };
 
+const UpcomingPage = () => {
+  const [data, setData] = useState();
+  const [rockets, setRockets] = useState();
+  const getUpcoming = async () => {
+    const api = "https://api.spacexdata.com/v4/launches/upcoming";
+    const res = await fetch(api).then((res) => res.json().then((res) => res));
+    setData(res);
+  };
+
+  const getRockets = async () => {
+    const api = 'https://api.spacexdata.com/v4/rockets'
+    const res = await fetch(api).then((res) => res.json().then((res) => res));
+    setRockets(res)
+  }
+
+  useEffect(() => {
+    getUpcoming();
+    getRockets()
+  }, []);
+  return (
+    <div>
+      <Upcoming data={data} rockets={rockets}/>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="App">
-      <Navbar items={["launches", "crews"]} />
-
+      <Navbar items={["upcoming", "launches", "crews"]} />
+      <WebVitasBox />
       <Routes>
-      <Route path="/" element={<></>} />
+        <Route path="/" element={<UpcomingPage />} />
+        <Route path="upcoming" element={<UpcomingPage />} />
         <Route path="crews" element={<CrewPage />} />
         <Route path="launches" element={<Launches />} />
       </Routes>
---
+++ src/components/Upcoming.jsx
@@ -0,0 +1,23 @@
+import Card from "./Card";
+
+const Upcoming = ({ data = [], rockets = [] }) => {
+    console.log(data)
+
+    return (
+        <div className='mx-6 flex flex-wrap'>
+            {data?.map((launch, index) =>
+                <div key={`${launch.id}-${index}`} className="md:basis-1/2 md:px-3">
+                    <Card key={index}
+                        mission_name={launch.name}
+                        mission_patch_small={launch.links.patch.small}
+                        launch_success={launch.success}
+                        launch_date_unix={launch.date_unix}
+                        rocket_name={rockets?.find(rocket => rocket.id === launch.rocket).name}
+                        ships_name={launch.fairings?.ships[0]?.name}
+                    />
+                </div>
+            )}
+        </div>
+    )
+}
+export default Upcoming
