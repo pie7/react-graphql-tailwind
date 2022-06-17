570be46e0f3ceeab22cc1980157c8f4fff013255
pie7 <j3pxy6@gmail.com>
2022-06-17 16:38:49 +0800

    fix: add toggleValue parameter

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -1,8 +1,13 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useState } from "react";
 
 function useToggle(defaultValue = false) {
-    const [isToggle, setIsToggle] = useState(defaultValue);
-    const handleToggle = useCallback(() => setIsToggle((isToggle) => !isToggle), []);
-    return [isToggle, handleToggle];
+  const [isToggle, setIsToggle] = useState(defaultValue);
+  const handleToggle = useCallback(
+    (toggleValue) => {
+      setIsToggle(toggleValue !== null ? toggleValue : !isToggle);
+    },
+    [isToggle]
+  );
+  return [isToggle, handleToggle];
 }
 export default useToggle;
